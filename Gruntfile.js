c8db3c2bdebe9154396bbef79272e87502638187
Jacob Boland <jboland@daptiv.com>
2013-10-17 15:42:19 -0700

    Correct description in grunt-task

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,5 +20,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
     // Default task.
-    grunt.registerTask('default', 'Build and run live tests and packaging', ['jshint']);
+    grunt.registerTask('default', 'Run jshint', ['jshint']);
 };
