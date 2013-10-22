0b244518db225e52a2c347ffa2a18ada79985c0f
Jacob Boland <jboland@daptiv.com>
2013-10-22 14:15:45 -0700

    Simplify npm test to just call grunt, including jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,7 +32,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-simple-mocha');
 
     // Default task.
-    grunt.registerTask('default', 'Run jshint', ['jshint', 'simplemocha:all']);
+    grunt.registerTask('default', 'Run jshint and all tests', ['jshint', 'simplemocha:all']);
 
     grunt.registerTask('teamcity', 'Run jshint', ['jshint', 'simplemocha:teamcity']);
 };
